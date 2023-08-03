9b3189c6137d9d9ffa3d067e19ddc244d201114c
Siroyoo <136694197+Siroyoo@users.noreply.github.com>
2023-08-03 12:58:57 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   src/App.js
    
    Changes not staged for commit:
    	deleted:    src/App.css
    	deleted:    src/Card_or_table.css
    	modified:   src/components/Header.js
    	deleted:    src/index.css
    	modified:   src/index.js
    	deleted:    src/pages/Card.js
    	modified:   src/pages/Home.js
    	deleted:    src/practice1/App.css
    	deleted:    src/practice1/App.js
    	deleted:    src/practice1/Card_or_table.css
    	deleted:    src/practice1/components/Body.js
    	deleted:    src/practice1/components/Footer.js
    	deleted:    src/practice1/components/Header.js
    	deleted:    src/practice1/pages/Card.js
    	deleted:    src/practice1/pages/CheckBox.js
    	deleted:    src/practice1/pages/Class.js
    	deleted:    src/practice1/pages/Counter.js
    	deleted:    src/practice1/pages/Form.js
    	deleted:    src/practice1/pages/FormObjects.js
    	deleted:    src/practice1/pages/FormTest.js
    	deleted:    src/practice1/pages/Home.js
    	deleted:    src/practice1/pages/InputReset.js
    	deleted:    src/practice1/pages/Scoreborard.js
    	deleted:    src/practice1/pages/Updater.js
    	deleted:    src/practice1/pages/classDB.json
    	deleted:    src/practice1/pages/profile.js
    	deleted:    src/practice1/statics/db.json
    	deleted:    src/practice1/statics/db2.json
    
     Untracked files:
    	src/css/
    	src/pages/Join.js
    	src/pages/Reserve.js
    	src/pages/Reserve_check.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // 리액트의 사용하는 이유는 모든 파일을 컴퍼넌트 할 수 있음
 // 나눈 애들을 재사용 할 수 있어서 사용함
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
+import "./css/App.css";
 import Home from "./pages/Home";
-import Blogs from "./pages/Blogs";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import BlogCreate from "./pages/BlogCreate";
+import Reserve_check from "./pages/Reserve_check";
+import Join from "./pages/Join";
 
 function App() {
   return (
@@ -16,8 +16,8 @@ function App() {
         <div className="continer">
           <Routes>
             <Route path="/" exact element={<Home />} />
-            <Route path="/blogs" exact element={<Blogs />} />
-            <Route path="/blogs/create" exact element={<BlogCreate />} />
+            <Route path="/reserve_check" exact element={<Reserve_check />} />
+            <Route path="/join" exact element={<Join />} />
             {/* <Route path="/menu" exact element={<Menu />} /> */}
           </Routes>
         </div>
